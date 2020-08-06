53162c47ce432b0bbff216e1235b4a6844dc1f0c
Rob Raesemann <rob@raesemann.com>
2020-08-06 08:29:26 -0400

    Testmaster.py has new SOEHandler code to get analog and binary values and class has new attribute to share data.

--- testmaster.py
+++ testmaster.py
@@ -7,7 +7,10 @@ log_handler = asiodnp3.ConsoleLogger().Create()
 manager = asiodnp3.DNP3Manager(1, log_handler)
 retry = asiopal.ChannelRetry().Default()
 listener = asiodnp3.PrintingChannelListener().Create()
-channel = manager.AddTCPClient('client', opendnp3.levels.NOTHING, retry, '192.168.69.151', '0.0.0.0', 20000, listener)
+channel = manager.AddTCPClient('client', opendnp3.levels.NOTHING, retry, '10.119.228.83', '0.0.0.0', 20000, listener)
+
+
+
 
 SLEEP_SECONDS = 5
 
@@ -16,19 +19,53 @@ SLEEP_SECONDS = 5
 # a custom implementation because the default printing one is
 # not so useful
 class SOEHandler(opendnp3.ISOEHandler):
+    
+
+    def _setValues(self, values):
+        self._values = values
+
+    def _getValues(self):
+        return self._values
+
+    values = property(_getValues, _setValues)
+
     def __init__(self):
         super(SOEHandler, self).__init__()
 
     def Process(self, info, values):
         print(f'values {values.Count()}\ttype: {type(values)}')
-       
+
+        global analog_values
+        global binary_values
+
+        a_vals = []
+        b_vals = []
+               
         if (type(values) == opendnp3.ICollectionIndexedAnalog):
             class BOSVisitor(opendnp3.IVisitorIndexedAnalog):
                 def __init__(self):
                     super(BOSVisitor, self).__init__()
                 def OnValue(self, indexed_instance):
-                    print(indexed_instance.index, indexed_instance.value.value)
+                    print(f'({indexed_instance.index}, {indexed_instance.value.value})')
+                    a_vals.append(indexed_instance.value.value)
             values.Foreach(BOSVisitor())
+            analog_values = a_vals.copy()
+            self.values['analog'] = a_vals.copy()
+
+        if (type(values) == opendnp3.ICollectionIndexedBinary):
+            class BOSVisitorBin(opendnp3.IVisitorIndexedBinary):
+                def __init__(self):
+                    super(BOSVisitorBin, self).__init__()
+                def OnValue(self, indexed_instance):
+                    print(f'({indexed_instance.index}, {indexed_instance.value.value})')
+                    b_vals.append(indexed_instance.value.value)
+            values.Foreach(BOSVisitorBin())
+            binary_values = b_vals.copy()
+            self.values['binary'] = b_vals.copy()
+
+        print(f'a_vals: {len(a_vals)} b_vals: {len(b_vals)}')
+        print(f'a: {len(analog_values)} b: {len(binary_values)}')
+    
 
     def Start(self):
         # This is implementing an interface, so this function
@@ -40,12 +77,27 @@ class SOEHandler(opendnp3.ISOEHandler):
         # must be declared.
         pass
 
+
+analog_values = []
+binary_values = []
+values = {
+    'analog': analog_values,
+    'binary': binary_values
+}
+# values['analog'] = analog_values
+# values["binary"] = binary_values
+
+
+
+
 soe_handler = SOEHandler()
+soe_handler.values = values
+new_val = soe_handler.values
 
 master_application = asiodnp3.DefaultMasterApplication().Create()
 stack_config = asiodnp3.MasterStackConfig()
 stack_config.master.responseTimeout = openpal.TimeDuration().Seconds(2)
-stack_config.link.RemoteAddr = 10
+stack_config.link.RemoteAddr = 2
 master = channel.AddMaster('master', soe_handler, master_application, stack_config)
 master.Enable()
 
@@ -55,8 +107,11 @@ time.sleep(SLEEP_SECONDS)
 # master.ScanRange(group_variation, 0, NUMBER_OF_OUTPUTS)
 
 while(1):
-    time.sleep(120)
-
+    time.sleep(10)
+    # master.AddClassScan(opendnp3.ClassField().AllClasses(),
+    #                                               openpal.TimeDuration().Minutes(30),
+    #                                               opendnp3.TaskConfig().Default())
+    print(f"Analog Value Count: {len(values['analog'])}  Binary: {len(values['binary'])}")
 
 
 master.Disable()
